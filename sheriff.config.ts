9d163419570d5d8af0d7771622e9e3bea161f48b
Gustavo Henrique do Espirito Santo <gustavohenriquedoespirito@gmail.com>
2026-03-24 01:31:14 -0300

    sheriff

--- sheriff.config.ts
+++ sheriff.config.ts
@@ -3,29 +3,18 @@ import { sameTag, SheriffConfig } from '@softarc/sheriff-core';
 export const config: SheriffConfig = {
   version: 1,
   tagging: {
-    'src/app': ['root'],
-    'src/app/shared/ui': ['domain:shared', 'type:ui'],
-    'src/app/<domain>/feature-<name>': ['domain:<domain>', 'type:feature'],
-    'src/app/<domain>/ui-<name>': ['domain:<domain>', 'type:ui'],
-    'src/app/<domain>/data-access': ['domain:<domain>', 'type:data-access'],
+    'src/app':                                      ['root'],
+    'src/app/shared/ui':                            ['domain:shared', 'type:ui'],
+    'src/app/domains/<domain>/features/<name>':      ['domain:<domain>', 'type:feature'],
+    'src/app/domains/<domain>/ui/<name>':           ['domain:<domain>', 'type:ui'],
+    'src/app/domains/<domain>/data-access':         ['domain:<domain>', 'type:data-access'],
   },
   depRules: {
-    // root acessa tudo
-    'root': ['*'],
-
-    // cada domínio só acessa akkl si mesmo e shared
-    'domain:*': [sameTag, 'domain:shared'],
-
-    // feature acessa ui, data-access e shared
-    'type:feature': ['type:ui', 'type:data-access', 'domain:shared'],
-
-    // ui acessa apenas shared
-    'type:ui': ['domain:shared'],
-
-    // data-access acessa apenas shared
+    'root':             ['*'],
+    'domain:*':         [sameTag, 'domain:shared'],
+    'type:feature':     ['type:ui', 'type:data-access', 'domain:shared'],
+    'type:ui':          ['domain:shared'],
     'type:data-access': ['domain:shared'],
-
-    // shared não depende de ninguém
-    'domain:shared': [],
+    'domain:shared':    [],
   },
 };
